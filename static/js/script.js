0008224fdd2f0fb069c688dbd7c3982bb2f073f4
Renata Kuroda <renatakuroda@gmail.com>
2023-03-04 11:45:31 +1100

    adding js function to create new ingredients and instructions input

---
+++ static/js/script.js
@@ -0,0 +1,49 @@
+let ingredientCount = 1;
+
+document.getElementById("add_ingredient").addEventListener("click", function() {
+    ingredientCount++;
+    const newIngredientDiv = document.createElement("div");
+    newIngredientDiv.classList.add("ingredient");
+
+    const newLabel = document.createElement("label");
+    newLabel.setAttribute("for", "ingredient_name_" + ingredientCount);
+    newLabel.textContent = "Ingredient " + ingredientCount + ":";
+
+    const newInput = document.createElement("input");
+    newInput.setAttribute("type", "text");
+    newInput.setAttribute("id", "ingredient_name_" + ingredientCount);
+    newInput.setAttribute("name", "ingredient_name[]");
+    newInput.required = true;
+
+    newIngredientDiv.appendChild(newLabel);
+    newIngredientDiv.appendChild(newInput);
+    document.getElementById("ingredients-container").appendChild(newIngredientDiv);
+});
+
+
+let stepCount = 1;
+const addStepButton = document.getElementById("add_step");
+const stepsContainer = document.getElementById("steps-container");
+
+addStepButton.addEventListener("click", function() {
+    stepCount++;
+
+    const newStepDiv = document.createElement("div");
+    newStepDiv.classList.add("step");
+
+    const newStepLabel = document.createElement("label");
+    newStepLabel.textContent = "Step " + stepCount + ":";
+
+    const newStepInstructionTextarea = document.createElement("textarea");
+    newStepInstructionTextarea.setAttribute("id", "step_instruction_" + stepCount);
+    newStepInstructionTextarea.setAttribute("name", "step_instruction[]");
+    newStepInstructionTextarea.required = true;
+
+    newStepDiv.appendChild(newStepLabel);
+    newStepDiv.appendChild(newStepInstructionTextarea);
+    stepsContainer.appendChild(newStepDiv);
+});
+
+
+const newStepLabel = document.createElement("label");
+newStepLabel.textContent = "Step " + stepCount + " (" + (stepCount - 1) + " completed):";
